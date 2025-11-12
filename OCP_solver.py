07b060b2b8aa752aab42faec5f10ed25b04bdea8
willem_esterhuizen <willem.e@r2.energy>
2025-11-12 09:13:11 +0200

    WIP: many improvements

--- OCP_solver.py
+++ OCP_solver.py
@@ -1,21 +1,22 @@
 import numpy as np
-from car_OCP import plot_solution, solve_OCP
+from car_OCP import plot_solution_in_state_space, solve_OCP, get_initial_warm_start, get_arc_length, plot_solution_versus_time, plot_control, plot_control_and_state_space
 
 if __name__ == "__main__":
     x_init = np.array([[2],[0],[np.pi/2]])
-
-    T = 8 # Horizon length
-    h = 0.1 # RK4 time step
     x_target = np.array([[-2],[0]])
 
+    T = 10 # Horizon length
+    h = 0.1 # RK4 time step
+
     obstacles = [
-        {'centre': (0,0), 'radius': 0.5},
-        {'centre': (-0.5,-0.5), 'radius': 0.2},
-        {'centre': (1,0.5), 'radius': 0.3},
-        {'centre': (1,-0.2), 'radius': 0.2},
-        {'centre': (-1.5,-0.2), 'radius': 0.2},
-        {'centre': (1.5,0), 'radius': 0.2},
-        {'centre': (-0.75,0), 'radius': 0.2}
+        {'centre': (0,0), 'radius': 0.5}, # this one and... 
+        {'centre': (-0.5,-0.5), 'radius': 0.2}, # this one for chattering, and         eps_cost=1e-5, eps_u=eps
+        # {'centre': (1,0.5), 'radius': 0.3},
+        # {'centre': (1,-0.2), 'radius': 0.2},
+        # {'centre': (-1.5,-0.2), 'radius': 0.2},
+        # {'centre': (1.5,0), 'radius': 0.2},
+        # {'centre': (-0.75,0), 'radius': 0.2},
+        # {'centre': (-1.25,0.2), 'radius': 0.2}
     ]
 
     constraints = {
@@ -25,8 +26,41 @@ if __name__ == "__main__":
         'u2_max' : 1,
     }
 
-    x_opt, u_opt = solve_OCP(x_init, x_target, obstacles, constraints, T, h)
-    print('////////')
-    print(f'x_final: {x_opt[:, -1]}, u_final: {u_opt[:, -1]}')
-    plot_solution(x_opt, u_opt.reshape(2, int(np.floor(T/h))), obstacles)
+    # warm_start = get_initial_warm_start(x_init, x_target, T, h)
+    warm_start=None
+    for eps in [0]:
+        eps_cost=1e-5
+        eps_u=eps
+        print('////////////////////////////')
+        print(f'eps_cost: {eps_cost}. eps_u: {eps_u}')
+        print('////////////////////////////')
+        eps_cost=eps_cost
+        eps_u=eps_u
+        x_opt, u_opt = solve_OCP(x_init, x_target, obstacles, constraints, T, h, warm_start, eps_u=eps_u, eps_cost=eps_cost, sqrt_cost_flag=True)
+
+        warm_start = {
+            'x1_warm': x_opt[0,:],
+            'x2_warm': x_opt[1,:],
+            'x3_warm': x_opt[2,:],
+            'u1_warm': u_opt[0,:],
+            'u2_warm': u_opt[1,:]
+        }
+        # warm_start['x1_warm'] = x_opt[0,:]
+        # warm_start['x2_warm'] = x_opt[1,:]
+        # warm_start['x3_warm'] = x_opt[2,:]
+        # warm_start['u1_warm'] = u_opt[0,:]
+        # warm_start['u2_warm'] = u_opt[1,:]
+
+        # plot_solution_in_state_space(x_opt, obstacles, title=fr'Solution with $\varepsilon$ = {eps_cost}')
+        # plot_solution_versus_time(x_opt, u_opt)
+        # 'stop'
+
+    #     # arc_length = get_arc_length(x_opt)
+
+    # plot_solution_in_state_space(x_opt, obstacles, title=fr'Solution with $\varepsilon$ = {eps_cost}')
+    # plot_solution_versus_time(x_opt, u_opt)
+    # plot_control(u_opt)
+    print(f'///////// arc length: {get_arc_length(x_opt)} //////////')
+    plot_control_and_state_space(u_opt, x_opt, obstacles)
+    
     'stop'
--- car_OCP.py
+++ car_OCP.py
@@ -3,22 +3,22 @@ from matplotlib.ticker import MaxNLocator
 import numpy as np
 import casadi as ca
 
-def solve_OCP(x_init, x_target, obstacles, constraints, T, h):
-    n = 3 # state dimension
-    m = 2 # control dimension
+def solve_OCP(x_init, x_target, obstacles, constraints, T, h, warm_start=None, eps_u=1e-6, eps_cost=1e-6, sqrt_cost_flag=True):
+    n = 3
+    m = 2
 
     no_steps = int(ca.floor(T/h))
 
-    # Constraints for all k
     u1_max = constraints['u1_max']
     u1_min = constraints['u1_min']
     u2_max = constraints['u2_max']
     u2_min = constraints['u2_min']
 
     opti = ca.Opti()
-    x = opti.variable(n, no_steps+1)  # State trajectory
-    u = opti.variable(m, no_steps)    # Control trajectory
-    # z = opti.variable(1, no_steps)
+    x = opti.variable(n, no_steps+1)
+    u = opti.variable(m, no_steps)
+    if sqrt_cost_flag is False:
+        z = opti.variable(1, no_steps)    # If absolute value cost
 
     # Specify the initial condition
     opti.subject_to(x[:, 0] == x_init)
@@ -27,72 +27,137 @@ def solve_OCP(x_init, x_target, obstacles, constraints, T, h):
     for k in range(no_steps):
         for obstacle in obstacles:
             opti.subject_to((x[0, k] - obstacle['centre'][0])**2 + (x[1, k] - obstacle['centre'][1])**2 >= obstacle['radius']**2)
-        # cost += z[:, k] * h
-        # opti.subject_to(z[:, k] >= u[0, k])
-        # opti.subject_to(z[:, k] >= -u[0, k])
-
-        eps = 1e-2
-        eps_u = 1e-2
-        cost += (1/h) * ca.sqrt( (x[0, k+1] - x[0, k])**2 + (x[1, k+1] - x[1, k])**2 + eps) \
-                + eps_u*(u[0, k]**2 + u[1, k]**2)
+        
+        if sqrt_cost_flag is False:
+            cost += z[:, k] + eps_u*(u[0, k]**2 + u[1, k]**2)
+            opti.subject_to(z[:, k] >= np.sqrt(u[0, k]**2 + eps_cost) )
+        else:
+            cost += np.sqrt(eps_cost) * ( ca.sqrt( (x[0, k+1] - x[0, k])**2 + (x[1, k+1] - x[1, k])**2 + eps_cost) + eps_u*(u[0, k]**2 + u[1, k]**2) )
+            # cost += ( ca.sqrt( (x[0, k+1] - x[0, k])**2 + (x[1, k+1] - x[1, k])**2 + eps_cost) + eps_u*(u[0, k]**2 + u[1, k]**2) )
 
         x_next = get_x_next(x[:, k], u[:, k], h)
         opti.subject_to(x[:, k+1] == x_next)
-
-    # cost += (x[0, -1] - x_target[0, -1])**2 + (x[1, -1] - x_target[1, -1])**2
+    # cost += 1e3*(x[0, -1] - x_target[0, -1])**2 + (x[1, -1] - x_target[1, -1])**2
     opti.subject_to(x[0, -1] == x_target[0, -1])
     opti.subject_to(x[1, -1] == x_target[1, -1])
 
-    # constraints for every time step
     opti.subject_to(opti.bounded(u1_min, u[0,:], u1_max))
     opti.subject_to(opti.bounded(u2_min, u[1,:], u2_max))
 
-    opts = {"ipopt.print_level": 5, "print_time": 0}
+    opts = {"ipopt.print_level": 5, "print_time": 1, "ipopt.sb": "yes"}
     # opts = {
     #     "ipopt.print_level": 5,
     #     "ipopt.tol": 1e-10,  # Tighter tolerance for precision
     #     "ipopt.mu_strategy": "adaptive",  # Better barrier handling
     #     "ipopt.hessian_approximation": "limited-memory",  # For large problems
     #     "ipopt.bound_relax_factor": 1e-6,  # Slight bound relaxation
-    #     "expand": True
+    #     "expand": True,
+    #     "ipopt.max_iter": 5000
     # }
 
-    # NEXT TIME: fix initial guess
-    initial_guess = get_initial_guess(x_init, x_target, no_steps)
+    if warm_start is not None:
+        opti.set_initial(u, np.vstack((warm_start['u1_warm'],
+                                       warm_start['u2_warm'])))
+        opti.set_initial(x, np.vstack((warm_start['x1_warm'],
+                                       warm_start['x2_warm'],
+                                       warm_start['x3_warm'])))
 
-    opti.set_initial(x, np.vstack((initial_guess['x1_guess'],
-                                   initial_guess['x2_guess'],
-                                   initial_guess['x3_guess'])))
-    opti.set_initial(u, np.vstack((initial_guess['u1_guess'],
-                                   initial_guess['u2_guess'])))
 
     opti.minimize(cost)
     opti.solver("ipopt", opts)
-    
 
-    
+    # check_conditioning_sqrt_cost(opti, warm_start, no_steps, n, m)
+    # check_conditioning_abs_cost(opti, warm_start, no_steps, n, m)
+
     solution = opti.solve()
     x_opt = solution.value(x)
     u_opt = solution.value(u)
 
     return x_opt, u_opt
 
-def get_initial_guess(x_init, x_target, no_steps):
-    x1_guess = np.linspace(0, x_target[0], no_steps)
-    x2_guess = np.linspace(0, x_target[1], no_steps)
-    x3_guess = np.linspace(0, 0, no_steps)
+def get_initial_warm_start(x_init, x_target, T, h):
+    no_steps = int(ca.floor(T/h))
+    x1_warm = np.linspace(0, x_target[0][0], no_steps + 1)
+    x2_warm = np.linspace(0, x_target[1][0], no_steps + 1)
+    x3_warm = np.linspace(0, 0, no_steps + 1)
 
-    u1_guess = np.ones(no_steps)
-    u2_guess = np.zeros(no_steps)
+    u1_warm = 0.1*np.ones(no_steps)
+    u2_warm = 0.1**np.ones(no_steps)
 
     return {
-        'x1_guess' : x1_guess,
-        'x2_guess' : x2_guess,
-        'x3_guess' : x3_guess,
-        'u1_guess' : u1_guess,
-        'u2_guess' : u2_guess
+        'x1_warm' : x1_warm,
+        'x2_warm' : x2_warm,
+        'x3_warm' : x3_warm,
+        'u1_warm' : u1_warm,
+        'u2_warm' : u2_warm
     }
 
+def check_conditioning_abs_cost(opti, warm_start, no_steps, n, m):
+    G = opti.g
+    F = opti.f
+    X = opti.x
+
+    print("Number of decision variables:", X.shape)
+    print("Number of constraints:", G.shape)
+
+    # Convert to callable CasADi function
+    J = ca.jacobian(G, X)
+    H = ca.hessian(F, X)[0]
+
+    nlp_fun = ca.Function(
+        "nlp_fun",
+        [X],  # inputs: decision vector
+        [F, G, J, H]
+    )
+    u0 = np.vstack((warm_start['u1_warm'],
+                    warm_start['u2_warm']))
+    x0 = np.vstack((warm_start['x1_warm'],
+                    warm_start['x2_warm'],
+                    warm_start['x3_warm']))
+    z0 = np.ones(no_steps)
+    X0 = np.vstack((u0.reshape(m*no_steps,1), x0.reshape(n*(no_steps+1),1), z0.reshape(1*(no_steps),1)))
+    f0, g0, J0, H0 = nlp_fun(X0)
+
+    print("Objective (f) magnitude:", float(abs(f0)))
+    print("Constraint (g) range: [", float(np.min(g0)), ",", float(np.max(g0)), "]")
+    print("Jacobian (∂g/∂x) coefficient range: [", np.min(J0), ",", np.max(J0), "]")
+    print("Hessian (∂²f/∂x²) coefficient range: [", np.min(H0), ",", np.max(H0), "]")
+
+def check_conditioning_sqrt_cost(opti, warm_start, no_steps, n, m):
+    G = opti.g
+    F = opti.f
+    X = opti.x
+
+    print("Number of decision variables:", X.shape)
+    print("Number of constraints:", G.shape)
+
+    # Convert to callable CasADi function
+    J = ca.jacobian(G, X)
+    H = ca.hessian(F, X)[0]
+
+    nlp_fun = ca.Function(
+        "nlp_fun",
+        [X],  # inputs: decision vector
+        [F, G, J, H]
+    )
+    u0 = np.vstack((warm_start['u1_warm'],
+                    warm_start['u2_warm']))
+    x0= np.vstack((warm_start['x1_warm'],
+                    warm_start['x2_warm'],
+                    warm_start['x3_warm']))
+    X0 = np.vstack((u0.reshape(m*no_steps,1),x0.reshape(n*(no_steps+1),1)))
+    f0, g0, J0, H0 = nlp_fun(X0)
+
+    print("Objective (f) magnitude:", float(abs(f0)))
+    print("Constraint (g) range: [", float(np.min(g0)), ",", float(np.max(g0)), "]")
+    print("Jacobian (∂g/∂x) coefficient range: [", np.min(J0), ",", np.max(J0), "]")
+    print("Hessian (∂²f/∂x²) coefficient range: [", np.min(H0), ",", np.max(H0), "]")
+
+def get_arc_length(x):
+    arc_length = 0
+    for k in range(x.shape[1]):
+        arc_length += np.linalg.norm(x[:, k])
+    return arc_length
 
 def f(x,u):
     return ca.vertcat(u[0]*ca.cos(x[2]),
@@ -130,9 +195,9 @@ def plot_solution_hold_on(ax, x_tot):
     ax.set_ylabel('Velocity')
     ax.grid(True)
 
-def plot_solution(x_tot, u_tot, obstacles):
+def plot_solution_versus_time(x_tot, u_tot):
     # this takes lists of numpy arrays...
-    fig = plt.figure(figsize=(10, 8))
+    fig = plt.figure(figsize=(8, 8))
     gs = fig.add_gridspec(5, 1)
     ax1 = fig.add_subplot(gs[0])
     ax2 = fig.add_subplot(gs[1], sharex=ax1)
@@ -151,6 +216,9 @@ def plot_solution(x_tot, u_tot, obstacles):
     x3 = np.append(x3[0], x3)
     time = np.arange(x_tot.shape[1] + 1) # because we duplicate the initial state
 
+    ax1.set_title("x_1 vs time", fontsize=14)
+    ax2.set_title("x_2 vs time", fontsize=14)
+    ax3.set_title("x_3 vs time", fontsize=14)
     ax1.step(time, x1, 'b-', label='x1')
     ax2.step(time, x2, 'b-', label='x2')
     ax3.step(time, x3, 'b-', label='x3 (heading)')
@@ -160,13 +228,13 @@ def plot_solution(x_tot, u_tot, obstacles):
     ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax3.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax1.set_ylabel('x1')
+    # ax1.set_ylabel('x1')
     ax1.legend()
     ax1.grid(True)
-    ax2.set_ylabel('x2')
+    # ax2.set_ylabel('x2')
     ax2.legend()
     ax2.grid(True)    
-    ax3.set_ylabel('x3 (heading)')
+    # ax3.set_ylabel('x3 (heading)')
     ax3.legend()
     ax3.grid(True)
     
@@ -175,24 +243,33 @@ def plot_solution(x_tot, u_tot, obstacles):
     u2 = u_tot[1]
     time = np.arange(u_tot.shape[1] + 1)
     ax4.step(time, np.append(u1[0], u1), 'g-', label='u1 (velocity)')
+    ax4.set_title("u_1 vs time", fontsize=14)
     ax4.set_xlabel('Time step')
-    ax4.set_ylabel('Control input')
+    # ax4.set_ylabel('Control input')
     ax4.legend()
     ax4.grid(True)
     ax5.step(time, np.append(u2[0], u2), 'g-', label='u2 (turning rate)')
+    ax5.set_title("u_2 vs time", fontsize=14)
     ax5.set_xlabel('Time step')
-    ax5.set_ylabel('Control input')
+    # ax5.set_ylabel('Control input')
     ax5.legend()
     ax5.grid(True)
+    fig.tight_layout(rect=[0, 0, 1, 0.95])
+    
+    # plt.savefig("ocp-open-loop.svg", format="svg")
+    plt.show()
 
-    # Plot the position in state space
-    fig = plt.figure(figsize=(10, 8))
+def plot_solution_in_state_space(x_tot, obstacles, title):
+    x1 = x_tot[0]
+    x2 = x_tot[1]
+    x3 = x_tot[2]
+    fig = plt.figure(figsize=(8, 8))
     gs_position_space = fig.add_gridspec(1, 1)
     ax6 = fig.add_subplot(gs_position_space[0])
-    ax6.plot(x1, x2, 'k-', label='Position')
+    ax6.set_title(title, fontsize=14)
+    ax6.plot(x1, x2, 'k-')
     ax6.set_xlabel('x1')
     ax6.set_ylabel('x2')
-    ax6.legend()
     ax6.grid(True)
     ax6.set_aspect('equal')
 
@@ -206,6 +283,68 @@ def plot_solution(x_tot, u_tot, obstacles):
         y = obstacle['centre'][1] + obstacle['radius'] * np.sin(theta)
         ax6.plot(x, y, 'k-')
 
-    plt.tight_layout()
-    # plt.savefig("ocp-open-loop.svg", format="svg")
+    fig.tight_layout(rect=[0, 0, 1, 0.95])
+
+def plot_control(u_tot):
+    fig = plt.figure(figsize=(8, 8))
+    gs = fig.add_gridspec(2, 1)
+    ax1 = fig.add_subplot(gs[0])
+    ax2 = fig.add_subplot(gs[1], sharex=ax1)
+
+    # Plot control versus time
+    u1 = u_tot[0]
+    u2 = u_tot[1]
+    time = np.arange(u_tot.shape[1] + 1)
+    ax1.step(time, np.append(u1[0], u1), 'g-', label='u1 (velocity)')
+    ax1.set_title("u_1 vs time", fontsize=14)
+    ax1.set_xlabel('Time step')
+    ax1.legend()
+    ax1.grid(True)
+    ax2.step(time, np.append(u2[0], u2), 'g-', label='u2 (turning rate)')
+    ax2.set_title("u_2 vs time", fontsize=14)
+    ax2.set_xlabel('Time step')
+    ax2.legend()
+    ax2.grid(True)
+    fig.tight_layout(rect=[0, 0, 1, 0.95])
+
+    plt.show()
+
+def plot_control_and_state_space(u_tot, x_tot, obstacles):
+    fig = plt.figure(figsize=(8, 8))
+    gs = fig.add_gridspec(3, 1)
+    ax1 = fig.add_subplot(gs[0])
+    ax2 = fig.add_subplot(gs[1])
+    ax3 = fig.add_subplot(gs[2], sharex=ax2)
+
+    x1 = x_tot[0]
+    x2 = x_tot[1]
+    ax1.plot(x1, x2, 'k-')
+    ax1.set_xlabel('x1')
+    ax1.set_ylabel('x2')
+    ax1.plot([x1[0], x1[-1]], [x2[0], x2[-1]], 'k.')
+    ax1.grid(True)
+    ax1.set_aspect('equal')
+    ax1.set_title('State space')
+
+    theta = np.linspace(0, 2*np.pi, 50)
+    for obstacle in obstacles:
+        x = obstacle['centre'][0] + obstacle['radius'] * np.cos(theta)
+        y = obstacle['centre'][1] + obstacle['radius'] * np.sin(theta)
+        ax1.plot(x, y, 'k-')
+
+    u1 = u_tot[0]
+    u2 = u_tot[1]
+    time = np.arange(u_tot.shape[1] + 1)
+    ax2.step(time, np.append(u1[0], u1), 'g-', label='u1 (velocity)')
+    ax2.set_title("u_1 vs time", fontsize=14)
+    ax2.set_xlabel('Time step')
+    ax2.legend()
+    ax2.grid(True)
+    ax3.step(time, np.append(u2[0], u2), 'g-', label='u2 (turning rate)')
+    ax3.set_title("u_2 vs time", fontsize=14)
+    ax3.set_xlabel('Time step')
+    ax3.legend()
+    ax3.grid(True)
+    fig.tight_layout(rect=[0, 0, 1, 0.95])
+
     plt.show()
